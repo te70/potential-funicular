629637e66e38b1eb1a11b59b1a6e45ce2707943b
te70 <tevinkinuthia@gmail.com>
2022-10-25 20:36:50 +0300

    Add whoami api

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get("/api/whoami", (req, res) => {
+  const ip = req.ip;
+
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.send({ ip: ip, language: language, software: software});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
