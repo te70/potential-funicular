528e48d2c7d691dae53770051d28f698c8afe4fc
te70 <tevinkinuthia@gmail.com>
2022-10-26 23:23:16 +0300

    Initial commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+//require and use "multer"
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +15,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanlyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err){
+    res.send(400);
+  }
+})
 
 
 const port = process.env.PORT || 3000;
